fc51c1203e105f27bbebe24327d3f5dcc4111783
Donald Wu <donaldwumail@gmail.com>
2015-04-12 22:13:12 -0700

    Created CSV opener for data

---
+++ data_opener.py
@@ -0,0 +1,16 @@
+import csv
+
+manigga = 0
+with open('crimes_since_2009.csv', 'rb') as csvfile:
+     crimereader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     for row in crimereader:
+     	print row
+       	for word in row:
+       		print word 
+        	manigga = manigga + 1 
+     	if manigga > 100:
+     		break
+
+
+#if __name__ == "__main__": 
+ #   main()
